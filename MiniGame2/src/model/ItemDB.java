eef0a27fc07e1a2bcd53a6e06f067d4c5d3580ab
mconn <mconn@Michaels-Laptop>
2022-03-02 16:41:15 -0500

    Item and RoomDB 0.5

--- MiniGame2/src/model/ItemDB.java
+++ MiniGame2/src/model/ItemDB.java
@@ -1,24 +1,39 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import controller.Item;
 import gameExceptions.GameException;
 
 public class ItemDB {
 	private static ItemDB instance;
 	private List<Item> items;
 	
-	private ItemDB() throws GameException {}
+	private ItemDB() throws GameException {
+		this.items = new ArrayList<>();
+	}
 	
 	public static ItemDB getInstance() throws GameException{
+		if(instance == null) {
+			instance = new ItemDB();
+		}
 		
+		return instance;
 	}
 	
 	public Item getItem(int id) throws GameException {
+		Optional<Item> item = items.stream().filter(i -> i.getItemID() == id).findFirst();
 		
+		if(item.isEmpty()){
+			throw new GameException("Item doesn't exit");
+		}
+		
+		return item.get();
 	}
 	
 	public void readItems() throws GameException{
-		
+		items.forEach(i -> System.out.println(i.toString()));
 	}
 }
--- MiniGame2/src/model/RoomDB.java
+++ MiniGame2/src/model/RoomDB.java
@@ -1,29 +1,56 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import controller.Item;
+import controller.Room;
 import gameExceptions.GameException;
 
 public class RoomDB {
 	private static RoomDB instance;
 	private List<Room> rooms;
 	
-	private RoomDB() throws GameException{}
+	private RoomDB() throws GameException{
+		this.rooms = new ArrayList<>();
+	}
 	
-	public List<Item> getItems() throws GameException{
+	public RoomDB getInstance() {
+		if(instance == null) {
+			instance = new RoomDB();
+		}
 		
+		return instance;
+	}
+	
+	public List<Item> getItems(int roomID) throws GameException{
+		Room room = getRoom(roomID);
+		ItemDB itemStore = ItemDB.getInstance();
+		
+		List<Item> items = room.getItems().stream().map(i -> itemStore.getItem(i)).collect(Collectors.toList());
+		
+		return items;
 	}
 	
 	public String getMap() {
-		
+		//TODO: returns a string representation of a mpa of the game?
+		return "";
 	}
 	
-	public Room getRoom() throws GameException {
+	public Room getRoom(int roomID) throws GameException {
+		Optional<Room> room = rooms.stream().filter(r -> r.getRoomID() == roomID).findFirst();
 		
+		if(room.isEmpty()) {
+			throw new GameException("Room doens't exist");
+		}
+		
+		return room.get();
 	}
 	
 	public void readRooms() throws GameException{
-		
+		//TODO: this method will read in the textfile and add rooms to the list
 	}
 	
 	public void updateRoom(Room rm) throws GameException {
